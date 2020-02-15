80a7c95e8e93a85309186859305b1420dd5da4ff
Mikael Medina <mikael.medina@hotmail.com>
2020-02-15 13:31:11 +0100

    Split doorOpen into two functions
    Signed-off-by: Mikael Medina <mikael.medina@hotmail.com>

--- source/doorControl.c
+++ source/doorControl.c
@@ -1,8 +1,10 @@
 #include "doorControl.h"
 
-void doorOpen(int floor, int open) {
-    if (floor) {
-        hardware_command_floor_indicator_on(floor);
-    }
-    hardware_command_door_open(open);
+void doorOpen(void) {
+    hardware_command_door_open(1);
+}
+
+
+void doorClose(void) {
+    hardware_command_door_open(0);
 }
--- source/doorControl.h
+++ source/doorControl.h
@@ -1,12 +1,13 @@
 #include "hardware.h"
 
 /**
- * @brief Commands the hardware to open the door and set the floor 
- *  indicator to the given floor.
+ * @brief Commands the hardware to open the door.
  *
- * @param floor int value of the desired floor indicator to turn on, 
- * if the value is 0 nothing happens.
- * @param open truthy value to open the door, 0 to close it. 
  */
-void doorOpen(int floor, int open);
+void doorOpen(void);
 
+/**
+ * @brief Commands the hardware to close the door.
+ *
+ */
+void doorClose(void);
