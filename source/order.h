d977fb5490c12033423ec6176b070e3288267279
Håvard Reierstad <havardreierstad@gmail.com>
2020-02-18 11:36:15 +0100

    Added some doxygen comments
    Signed-off-by: Håvard Reierstad <havardreierstad@gmail.com>

--- source/order.h
+++ source/order.h
@@ -8,18 +8,30 @@
 #define ORDER_H
 #include "hardware.h"
 
+/**
+ * @brief Polls the buttons and adds active buttons to lists.
+ */
 void orderPoll(void);
 
 /**
- * @brief 
+ * @brief Adds active button to list.
+ * 
+ * @param floor The floor of the button.
+ * 
+ * @param orderType Type of the order.
  */
 void orderAdd(int floor, HardwareOrder orderType);
 
 /**
- * @brief 
+ * @brief Clears all orders at given floor.
+ * 
+ * @param floor The floor to be cleared of orders.
  */
 void orderClear(int floor);
 
+/**
+ * @brief Clears all orders at all floors.
+ */
 void orderClearAll(void);
 
 #endif
