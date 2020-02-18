fd7c09147fb7814dfe11c7bfd2b3c581f354b206
Mikael Medina <mikael.medina@hotmail.com>
2020-02-18 09:55:29 +0100

    Basic functionality for obstruction and stop button
    Signed-off-by: Mikael Medina <mikael.medina@hotmail.com>

---
+++ source/emergency.c
@@ -0,0 +1,20 @@
+#include "emergency.h"
+
+int emergencyPollStop(void) {
+    if(hardware_read_stop_signal()) {
+        hardware_command_stop_light(1);
+        hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+        // Remove all orders from the queue
+        while(hardware_read_stop_signal()) {}
+        return 1;
+    }
+    hardware_command_stop_light(0);
+    return 0;
+}
+
+int emergencyPollObstruction(void) {
+    // && state is door open
+    if(hardware_read_obstruction_signal()) {
+        hardware_command_stop_light(1);
+    }
+}
---
+++ source/emergency.h
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @brief Library that handles the stop button and obstructions.
+ * 
+ */
+
+#ifndef EMERGENCY_H
+#define EMERGENCY_H
+#include "hardware.h"
+
+/**
+ * @brief Polls the stop button, sets stop light active and stops
+ *  the elevator if button is pressed. Also removes all orders.
+ * @return 1 if the button was pressed, 0 otherwise.
+ */
+int emergencyPollStop(void);
+
+/**
+ * @brief Polls the obstruction switch, if there is an obstruction
+ * turns on the stoplight.
+ * @return 1 if there was an obstruction, 0 otherwise.
+*/
+int emergencyPollObstruction(void);
+
+#endif
